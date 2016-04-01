fdb6a1fc9484204609d5696b3bc16dbb924e8dbc
Chris Aporta <christopher.aporta@gmail.com>
2016-04-01 00:46:44 -0400

    complete lab

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(flavor) {
+  return "I love " + flavor;
+}
+
+function shouting(phrase) {
+  return phrase.toUpperCase();
+}
+
+function roundDown(n) {
+  return Math.floor(n);
+}
+
+function theTruth() {
+  return true;
+}
